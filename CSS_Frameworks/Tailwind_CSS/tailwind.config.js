ec6f1500dc9c23a5affbcebfaff63cc333131eff
Bhudevi Dobbala <bhudevi2461@gmail.com>
2024-12-03 12:41:31 +0530

    css frameworks

---
+++ CSS_Frameworks/Tailwind_CSS/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  darkMode: 'media',  // Automatically switches to dark mode based on system settings
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
